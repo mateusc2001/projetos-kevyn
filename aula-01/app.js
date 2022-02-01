bbb6d9126c792af4d115e7035910140d10a46134
mateusc2001 <mateusc2001@gmail.com>
2022-02-01 14:20:55 -0300

    aula 01

---
+++ aula-01/app.js
@@ -0,0 +1,6 @@
+function salvar() {
+    const valorInput1 = document.getElementById('input1').value;
+    const valorInput2 = document.getElementById('input2').value;    
+    alert(`Salvo com sucesso ${valorInput1} ${valorInput2}`);
+    // alert ('Salvo com sucesso'+valorInput1+valorInput2)
+}
